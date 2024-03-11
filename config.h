3f25a8e24ac1f4a06700393e10b4a37c9aa72ff6
Denis Davidoglu <dawidogg.feed@gmail.com>
2024-03-11 16:16:53 +0300

    Update config

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #define CONFIG
 
 const char *touchpad_path = "/dev/input/event6";
-const char *on_heart_detect_command = "gwenview /home/dawidogg/Pictures/gimp/pelmeni.png >nul 2>nul";
+const char *on_heart_detect_command = "gwenview /home/dawidogg/Pictures/gimp/pelmeni.png >nul 2>nul &";
 const int skip_writing = 1;
 const char *output_dir = "data_empty";
 const int ignore_first = 8;
